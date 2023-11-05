64b87647dd16d5abe83779aecd891339395cc641
webglBuilds <joyjack12@hotmail.com>
2023-11-06 01:00:47 +0200

    testing new init with whole project

---
+++ main.js
@@ -0,0 +1,92 @@
+import './style.css'
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+
+let camera, scene, renderer;
+
+init();
+render();
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 100 );
+    camera.position.set( 1.5, 4, 9 );
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xf6eedc );
+
+
+    const dirLight = new THREE.DirectionalLight( 0xffffff, 3 );
+				dirLight.color.setHSL( 0.1, 1, 0.95 );
+				dirLight.position.set( - 1, 1.75, 1 );
+				dirLight.position.multiplyScalar( 30 );
+				scene.add( dirLight );
+
+				dirLight.castShadow = true;
+
+				dirLight.shadow.mapSize.width = 2048;
+				dirLight.shadow.mapSize.height = 2048;
+
+				const d = 50;
+
+				dirLight.shadow.camera.left = - d;
+				dirLight.shadow.camera.right = d;
+				dirLight.shadow.camera.top = d;
+				dirLight.shadow.camera.bottom = - d;
+
+				dirLight.shadow.camera.far = 3500;
+				dirLight.shadow.bias = - 0.0001;
+
+    //
+
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath( 'jsm/libs/draco/gltf/' );
+
+    const loader = new GLTFLoader();
+    loader.setDRACOLoader( dracoLoader );
+    loader.setPath( 'models/' );
+    loader.load( 'MenuCompressed.glb', function ( gltf ) {
+       
+        scene.add( gltf.scene );
+
+        render();
+
+    } );
+
+    //
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.addEventListener( 'change', render );
+    controls.target.set( 0, 2, 0 );
+    controls.update();
+
+    window.addEventListener( 'resize', onWindowResize );
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    render();
+
+}
+
+//
+
+function render() {
+
+    renderer.render( scene, camera );
+
+}
